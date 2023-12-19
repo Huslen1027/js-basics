ec47e0291d6e478d91ae221f04be321480d6bfa3
Huslen Batzorig <23LP2437@G304-CC0026.local>
2023-12-19 09:54:26 +0800

    morning dec 19

---
+++ array/city.js
@@ -0,0 +1,37 @@
+let city = [
+  "Nalaih",
+  "Baganuur",
+  "tov",
+  "Ulaanbaatar",
+  "Suhbaatar",
+  "Khowsgol",
+  "Dornod",
+  "Hentii",
+]
+
+console.log(city[0]);
+console.log(city[1]);
+console.log(city[2]);
+console.log(city[3]);
+console.log(city[4]);
+console.log(city[5]);
+console.log(city[6]);
+console.log(city(7));
+
+let japanese = {
+  tokyo: { shibuya: "8,956,000" },
+  yokohama: { ohama: "3,690,000" },
+  osaka: { kashima: "2,670,000" },
+  nagoya: { tsuguru: "2,266,000" },
+  sapporo: { shibari: "1,883,027" },
+  kobe: { shinobi: "1,528,478" },
+  kyoto: { itachi: "1,459,640" },
+};
+
+console.log(japanese.tokyo.shibuya);
+// console.log(japenese.yokohama.ohama);
+// console.log(japanese.osaka.kashima);
+// console.log(japanese.nagoya.tsuguru);
+// console.log(japanese.sapporo.shibari);
+// console.log(japanese.kobe.shinobi);.
+// console.log(japanese.kyoto.itachi);
