8a050b96a4781a9cf01fd1b98ab4746958ffd3a6
Huslen1027 <hvslenhvslen566@gmail.com>
2023-12-21 12:39:46 +0800

    a

--- randomcolor.js/java.js
+++ randomcolor.js/java.js
@@ -51,24 +51,25 @@ const gamebody = document.createElement("div");
 gamebody.setAttribute("class", "gamebody");
 startBtn.addEventListener("click", startgame);
 root.appendChild(gamebody);
-function startgame() {
-  const box = document.createElement("div");
-  box.setAttribute("class", "box");
 
-  const red = Math.floor(Math.random() * 255);
-  const blue = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 205 + 50);
-  const rand = Math.floor(Math.random() * 8);
-  for (i = 0; i < 9; i++) {
-    const card = document.createElement("div");
-    card.setAttribute("class", "card");
-    if (i == rand) {
-      card.style.backgroundColor = `rgb(${red}, ${green - 50}, ${blue})`;
-    } else {
-      card.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+function startgame() {
+    const box = document.createElement("div");
+    box.setAttribute("class", "box");
+
+    const red = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 205 + 50);
+    const rand = Math.floor(Math.random() * 8);
+    for (i = 0; i < 9; i++) {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+        if (i == rand) {
+            card.style.backgroundColor = `rgb(${red}, ${green - 50}, ${blue})`;
+        } else {
+            card.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        }
+        box.appendChild(card);
     }
-    box.appendChild(card);
-  }
-  gamebody.innerHTML = "";
-  gamebody.appendChild(box);
+    gamebody.innerHTML = "";
+    gamebody.appendChild(box);
 }
